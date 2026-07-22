08802913594500be5d8b45ff7799357083fd2ecd
Benjamin Pol <polbenjamin7a@outlook.es>
2026-07-22 13:10:34 -0400

    v1.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'screens/main_layout.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
